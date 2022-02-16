ff1c615c69c6659508aeffc1b18418654ad1c9c5
Anna <romeborn@student.chalmers.se>
2022-02-16 11:39:21 +0100

    Better comments

--- experimenting/Transform.hs
+++ experimenting/Transform.hs
@@ -5,16 +5,17 @@ import Language.Haskell.Exts
 -- import           Data.Map   (Map)
 -- import qualified Data.Map as Map
 
--- perhaps what a state to keep track of the categories
+-- perhaps have a state to keep track of the categories
 data Env l = Env {
     categories :: [(Name l , [Name l])] -- Map Category [Name]
 }
 
+-- | Transform tree from parse result.
 transform :: ParseResult (Module SrcSpanInfo) -> ParseResult (Module SrcSpanInfo)
 transform f@ParseFailed{} = f
 transform (ParseOk ast) = ParseOk $ transformModule ast
 
-
+-- | Transform a module 
 transformModule :: Module SrcSpanInfo -> Module SrcSpanInfo
 transformModule (Module srcinfo mhead ps is ds) = do
     let ds' = map transformDecl ds
@@ -23,6 +24,7 @@ transformModule (Module srcinfo mhead ps is ds) = do
 transformModule xml = xml 
 -- ^ XmlPage and XmlHybrid formats not handled (yet)
 
+-- | Transform a top level declaration
 transformDecl :: Decl SrcSpanInfo -> Decl SrcSpanInfo
 transformDecl (PieceDecl srcinfo cat nam cs ders) = 
     -- add cat to list of categories
@@ -44,8 +46,9 @@ transformDecl (PieceDecl srcinfo cat nam cs ders) =
 
 transformDecl d = d
 
--- TODO: make sure this changes the recursive part to a parametric variable
--- i.e. Pair Expr Expr should become Pair a a
+{- | Parametrize a piece constructor to have a parametrized variable as recursive 
+    parameter instead of the name of the category it belongs to.
+-}
 parametConstructor :: Eq l => Name l -> Name l -> ConDecl l -> ConDecl l
 parametConstructor parname cat (ConDecl l1 cname types) = 
     ConDecl l1 cname (map (parametType parname) types)
@@ -57,9 +60,11 @@ parametConstructor parname cat (ConDecl l1 cname types) =
         parametType _ t = t
 parametConstructor _ _ c = c 
 
+-- | Wrap a ConDecl in a QualConDecl
 conDeclToQual :: ConDecl l -> QualConDecl l
 conDeclToQual c = QualConDecl (ann c) Nothing Nothing c
 
+-- | Create a Deriving functor for a given data type
 deriveFunctor :: Name l -> Name l -> l -> Deriving l 
 deriveFunctor nam parname aders =
   Deriving aders (Just (DerivStock aders))
@@ -81,12 +86,12 @@ deriveFunctor nam parname aders =
             (TyVar aders parname))))]
 
 
-
+-- TODO: Figure out how to handle names for parametrize variables
 getParName :: l -> Name l 
 getParName = undefined
 
 
-
+-- TODO: Check what should be derived by template Haskell, and build the corresponding tree.
 deriveTH = undefined
 -- Something like:
 -- take list of all categories and for all of them: 

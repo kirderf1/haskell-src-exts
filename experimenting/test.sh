eee1c9b0d90306ee335ae3679257190f69d04304
romeborn <romeborn@student.chalmers.se>
2022-03-01 17:15:13 +0100

    Folder for build output from test

--- experimenting/test.sh
+++ experimenting/test.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-#dist-newstyle/build/x86_64-linux/ghc-8.10.1/haskell-src-exts-1.23.1/x/program/build/program/program experimenting/tests/Common.hs > experimenting/Output.hs
-cabal -v0 run program experimenting/tests/Common.hs > experimenting/Output.hs
-ghc -o experimenting/output experimenting/Output.hs
-experimenting/output
+cabal -v0 run program experimenting/tests/Common.hs > experimenting/build/Output.hs
+ghc -o experimenting/build/output experimenting/build/Output.hs
+experimenting/build/output

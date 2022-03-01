adf98d6c49ec07420fe4b8becfb68d33bc4b5ced
romeborn <romeborn@student.chalmers.se>
2022-03-01 17:02:54 +0100

    Non-verbose cabal run

--- experimenting/test.sh
+++ experimenting/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cabal build
-dist-newstyle/build/x86_64-linux/ghc-8.10.1/haskell-src-exts-1.23.1/x/program/build/program/program experimenting/tests/Common.hs > experimenting/Output.hs
+#dist-newstyle/build/x86_64-linux/ghc-8.10.1/haskell-src-exts-1.23.1/x/program/build/program/program experimenting/tests/Common.hs > experimenting/Output.hs
+cabal -v0 run program experimenting/tests/Common.hs > experimenting/Output.hs
 ghc -o experimenting/output experimenting/Output.hs
 experimenting/output

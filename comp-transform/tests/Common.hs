8a06c81f99deaf759f4cc600fe4989ff8e1b2c9f
romeborn <romeborn@student.chalmers.se>
2022-03-02 14:02:18 +0100

    Add eval for Op in example Common

--- comp-transform/tests/Common.hs
+++ comp-transform/tests/Common.hs
@@ -9,15 +9,24 @@ data piece Expr ==> Value = Const Int -- | Pair Expr Expr
 data piece Expr ==> Op = Add Expr Expr | Mult Expr Expr -- | Fst Expr | Snd Expr
 
 -- Signature for the simple expression language
-type Sig = Expr ==> (Op, Value)
+type Sig = Expr ==> (Value, Neg)
 
 eval -: Expr -> Int
 
-{-
+
 eval for Value where 
     eval (Const i) = i
-    -}
+   
+eval for Op where
+    eval (Add e1 e2) = eval e1 + eval e2
+    eval (Mult e1 e2) = eval e1 * eval e2
 
+{-    
+ex :: Sig
+ex = iConst 2
+
+evalEx :: Int
+evalEx = eval ex-}
     
 main :: IO ()
-main = putStrLn "Det funkar!"
+main = putStrLn $ "Det funkar! " -- ++ show evalEx

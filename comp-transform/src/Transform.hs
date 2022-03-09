c26bc8227bbf0dd7b6c02b81a1bcd1b6ea500736
kirderf1 <kirderf4@gmail.com>
2022-03-09 16:16:56 +0100

    Remove additional derives as they should not always be present

--- comp-transform/src/Transform.hs
+++ comp-transform/src/Transform.hs
@@ -161,7 +161,7 @@ getParName = name "a"
 
 -- | Template Haskell derive for a piece
 deriveTHPiece :: Name () -> Decl () 
-deriveTHPiece pieceName = deriveTH pieceName ["makeEqF", "makeShowF", "smartConstructors"]
+deriveTHPiece pieceName = deriveTH pieceName ["smartConstructors"]
 
 -- | Template Haskell derive for a certain data type from a list of things to derive
 deriveTH :: Name () -> [String] -> Decl () 

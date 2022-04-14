8aae39b9c087d0cdbf96560da05bb3526b324201
kirderf1 <kirderf4@gmail.com>
2022-04-14 14:34:11 +0200

    Extract new module Names

--- comp-transform/src/Transform.hs
+++ comp-transform/src/Transform.hs
@@ -8,6 +8,7 @@ import Language.Haskell.Exts
 import Types
 import Decls
 import Exps
+import Names
 
 import           Data.Map   (Map)
 import qualified Data.Map as Map
@@ -487,62 +488,3 @@ qNameStr err (Qual _ _moduleName nam) = nameStr err nam
 qNameStr err (UnQual _ nam) = nameStr err nam
 qNameStr err _ = throwError $ "Unexpected special QName in " ++ err
 
-collectUniqueVars :: Type () -> [Name ()]
-collectUniqueVars = removeDups Set.empty . collectVars
-  where
-    removeDups _   []         = []
-    removeDups set (v:vars) =
-      if Set.member v set
-        then removeDups set vars
-        else v : removeDups (Set.insert v set) vars
-
-class WithVar a where
-    collectVars :: a l -> [Name l]
-
-instance WithVar Type where
-    collectVars t1 = case t1 of
-          TyForall _ mtvs mccx mcx t    -> concatMap (concatMap collectVars) mtvs ++ concatMap collectVars mccx 
-                                            ++ concatMap collectVars mcx ++ collectVars t
-          TyStar  _                     -> []
-          TyFun   _ t1' t2              -> collectVars t1' ++ collectVars t2
-          TyTuple _ _ ts                -> concatMap collectVars ts
-          TyUnboxedSum _ s              -> concatMap collectVars s
-          TyList  _ t                   -> collectVars t
-          TyParArray  _ t               -> collectVars t
-          TyApp   _ t1' t2              -> collectVars t1' ++ collectVars t2
-          TyVar   _ n                   -> [n]
-          TyCon   _ qn                  -> []
-          TyParen _ t                   -> collectVars t
-          TyInfix _ ta _ tb             -> collectVars ta ++ collectVars tb
-          TyKind  _ t k                 -> undefined
-          TyPromoted _   p              -> undefined
-          TyEquals _ a b                -> undefined
-          TySplice _ s                  -> undefined
-          TyBang _ b u t                -> undefined
-          TyWildCard _ n                -> undefined
-          TyQuasiQuote _ n s            -> undefined
-          TyComp _ c t                  -> []
-
-
-instance WithVar TyVarBind where
-    collectVars (KindedVar   _ n _k) = [n]
-    collectVars (UnkindedVar _ n)    = [n]
-
-instance WithVar Context where
-    collectVars (CxSingle _ asst ) = collectVars asst
-    collectVars (CxTuple  _ assts) = concatMap collectVars assts
-    collectVars (CxEmpty _) = []
-
-instance WithVar Asst where
-    collectVars asst = case asst of
-        TypeA _ t           -> collectVars t
-        IParam _ ipn t      -> undefined
-        ParenA _ a          -> collectVars a
-
-instance WithVar CompContext where
-    collectVars (CompCxSingle _ c ) = collectVars c
-    collectVars (CompCxTuple  _ cs) = concatMap collectVars cs
-    collectVars (CompCxEmpty _) = []
-    
-instance WithVar Constraint where
-    collectVars _ = undefined

cfb3ba823db6b86e3dde24b2dc8d1894958be92d
romeborn <romeborn@student.chalmers.se>
2022-05-16 14:30:55 +0200

    Modify test script to test with library file, and update file it tests

--- comp-transform/Common.hs
+++ comp-transform/Common.hs
@@ -9,21 +9,21 @@ data piece Expr ==> Value = Const Int -- | Pair Expr Expr
 data piece Expr ==> Op = Add Expr Expr | Mult Expr Expr -- | Fst Expr | Snd Expr
 
 -- Signature for the simple expression language
-type Sig = Expr ==> (Value, Op)
+type Sig = Expr ==> (Value | Op)
 
 eval -: Expr -> Int
 
 
-eval for Value where 
+ext eval for Value where 
     eval (Const i) = i
    
-eval for Op where
+ext eval for Op where
     eval (Add e1 e2) = eval e1 + eval e2
     eval (Mult e1 e2) = eval e1 * eval e2
 
 
 ex :: Sig
-ex = iConst 2 `iMult` iConst 5
+ex = Const 2 `Mult` Const 5
 
 evalEx :: Int
 evalEx = eval ex
